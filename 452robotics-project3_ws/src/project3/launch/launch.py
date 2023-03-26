6d986eafc69d08c0404a27ea436bafe50ee5fcb6
Nooby9 <1330762153@qq.com>
2023-03-26 18:30:00 -0500

    replaced need for playBag with launch file execute

--- 452robotics-project3_ws/src/project3/launch/launch.py
+++ 452robotics-project3_ws/src/project3/launch/launch.py
@@ -1,10 +1,26 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, TimerAction
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import *
 
 def generate_launch_description():
-    playBag = Node(package='project3',
-                executable='playBag')
+    # playBag = Node(package='project3',
+    #             executable='playBag')
     peopleCounter = Node(package='project3',
                 executable='peopleCounter')
-    ld = LaunchDescription([peopleCounter, playBag])
+    
+    
+    # declares new argument
+    bagIn = DeclareLaunchArgument('bagIn', default_value = 'example1') #command line is: ros2 launch project3 launch.py bagIn:=example1
+    # read in that new argument (not string)
+    bagInName = LaunchConfiguration('bagIn')
+    # concatenated path for example# 
+    bagPath = DeclareLaunchArgument('bagPath', default_value=['./src/project3/project3/bags/', bagInName])
+    
+    bagPathName = LaunchConfiguration('bagPath')
+    
+    
+    ep = ExecuteProcess(cmd=['ros2', 'bag', 'play', bagPathName])
+    
+    ld = LaunchDescription([bagIn, bagPath, ep, peopleCounter])
     return ld
